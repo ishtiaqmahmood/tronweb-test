9cae6941c38d17e115bfd226d6cf143a17d6680e
Ishtiaq Mahmood <ishtiaq@quanticdynamics.com>
2021-07-31 13:39:44 +0600

    Bear test

--- src/App.js
+++ src/App.js
@@ -107,7 +107,38 @@ class App extends React.Component {
         await this.setState({tronwebaddress : tmp_tronwebaddress});
         console.log("tronwebaddress", this.state.tronwebaddress);
 
+        let contract = await Utils.tronWeb
+        .contract()
+        .at("TS8ZXq5VqpLm7Yvh4kZTEkoM2q5rqM9j98")
+            let abi = contract.abi;
+            console.log(abi);
+        
+        // let result = await contract.usernameIsAvailable().call()
+        // console.log('result: ', result);
+
+        // try {
+        //     let result = await contract.trxToSun().call()
+        //     console.log('result: ', result);
+        // } catch(error) {
+        //     console.error("trigger smart contract error",error)
+        // }
+    
     }
+
+    triggerSmartContract() {
+        const Address = "TS8ZXq5VqpLm7Yvh4kZTEkoM2q5rqM9j98";//contract address
+    
+        try {
+            let contract = Utils.tronWeb.contract().at(Address);
+            //Use call to execute a pure or view smart contract method.
+            // These methods do not modify the blockchain, do not cost anything to execute and are also not broadcasted to the network.
+            let result = contract.getLeaderBoard().call();
+            console.log('result: ', result);
+        } catch(error) {
+            console.error("trigger smart contract error",error)
+        }
+    }
+    
 
     render() {
         if(!this.state.tronWeb.installed)
@@ -120,6 +151,7 @@ class App extends React.Component {
               <div className='row'>
                 <div className='col-lg-12 text-center' >
                   <p> Your Address : {this.state.tronwebaddress} </p>
+                  <button onClick={this.triggerSmartContract}>Click</button>
                   <br/>
                   <br/>
                 </div>
